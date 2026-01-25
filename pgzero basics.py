4f3e04ae38c36df1415455d1dcaa1a43d45bed4d
dog-loveraria123 <ariababa14@gmail.com>
2026-01-25 12:44:00 -0500

    Update pgzero basics.py

--- pgzero basics.py
+++ pgzero basics.py
@@ -1,16 +1,27 @@
 import pgzrun
+import random
 #screen dimensions
 WIDTH=500
 HEIGHT=400
 def draw():
+    w=500
+    h=200
+    screen.fill('black')
+    r=255
+    g=random.randint(0,255)
+    b=0
+    for i in range(25):
+        
     #changing the screen color
-    screen.fill('lavender')
-    a=Rect((250,200),(100,50))
-    a.center=250,200
-    #function for drawing rectangle
-    screen.draw.rect(a,'turquoise',width=3)
-    #square
-    b=Rect((300,300),(100,100))
-    screen.draw.filled_rect(b,('purple'))
+        a=Rect((250,200),(w,h))
+        a.center=250,200
+        #function for drawing rectangle
+        screen.draw.rect(a,(r,g,b))
+        r-=5
+        b+=5
+        w-=15
+        h+=7
+
+
 #showing the screen
 pgzrun.go()
