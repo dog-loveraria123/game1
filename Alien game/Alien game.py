612b4193e3a5c8e588cf35bd826211dab2f45a29
dog-loveraria123 <ariababa14@gmail.com>
2026-02-08 12:25:20 -0500

    Update Alien game.py

--- Alien game/Alien game.py
+++ Alien game/Alien game.py
@@ -5,15 +5,29 @@ HEIGHT=500
 a= Actor('dog.png')
 a.x=250
 a.y=250
+m='Dog Clicker Game'
 def draw():
     screen.fill('light blue')
     a.draw()
-    screen.draw.text('Dog Clicker Game',(30,20),fontsize=30,color='black')
+    screen.draw.text(m,(30,20),fontsize=30,color='black')
 # function for random position
 def rp():
     a.x=random.randint(50,450)
     a.y=random.randint(50,450)
+    clock.schedule(mc,2.0)
 clock.schedule_interval(rp,2.0)
+def mc():
+    global m 
+    m='You Missed!'
+    rp()
+def on_mouse_down(pos):
+    global m
+    if a.collidepoint(pos):
+        m='Good Job!'
+        clock.unschedule(mc)
+        rp()
+    else:
+        m='You Missed!'
 def update():
     pass
 pgzrun.go()
