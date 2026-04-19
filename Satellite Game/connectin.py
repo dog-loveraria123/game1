a0edff17504fe47d07a6c1af161c764687165742
dog-loveraria123 <ariababa14@gmail.com>
2026-04-19 12:32:57 -0400

    Update connectin.py

--- Satellite Game/connectin.py
+++ Satellite Game/connectin.py
@@ -17,7 +17,7 @@ def duplicate():
         satellite.x=random.randint(50,550)
         satellite.y=random.randint(50,300)
         sl.append(satellite)
-    st=time.time
+    st=time.time()
 def draw():
     global tt
     screen.blit('background.jpg',(0,0))
@@ -26,9 +26,24 @@ def draw():
         i.draw()
         screen.draw.text(str(cn),(i.pos[0],i.pos[1]-10),color='darkred')
         cn=cn+1
+    for ld in l:
+        screen.draw.line(ld[0],ld[1],'darkred')
     if sc<8:
         tt=(time.time())-st
-        screen.draw.text(str(tt),(0,0),color='pink')
+        screen.draw.text(str(round(tt,1)),(0,0),color='pink')
+    else:
+        screen.draw.text(str(round(tt,1)),(0,0),color='pink')
+#Function for Mouse Event
+def on_mouse_down(pos):
+    global sc,l
+    if sc<8:
+        if sl[sc].collidepoint(pos):
+            if sc:
+                l.append((sl[sc-1].pos,sl[sc].pos))
+            sc=sc+1
+        else:
+            l=[]
+            sc=0
 def update():
     pass
 duplicate()
