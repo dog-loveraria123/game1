ac82cf48ad859fb5947577550f983954f3184ca8
dog-loveraria123 <ariababa14@gmail.com>
2026-05-03 11:56:18 -0400

    Update falling game.py

--- New folder (2)/falling game.py
+++ New folder (2)/falling game.py
@@ -11,9 +11,12 @@ totallevels=10
 current=[]
 speed=5
 actualtarget=None
-listfall=['bear.png','bird.png','bunny.png','cat.png','Dog.png','tiger.png']
+listfall=['bear.png','bird.png','bunny.png','cat.png','dog.png','tiger.png']
+actors=[]
 def draw():
     screen.blit('main bg.jpg',(0,0))
+    for item in actors:
+        item.draw()
     #gameover/gamewin
     if go==True:
         screen.blit('lose bg.jpg',(0,0))
@@ -21,16 +24,30 @@ def draw():
         screen.blit('win bg.jpg',(0,0))
 #Function fo falling items
 def falling(levels):
-    global actualtarget
+    global actualtarget,actors
     temp=[]
     actualtarget=random.choice(listfall)
     #List for extra items
     extra=random.choices([i for i in listfall if i != actualtarget],k=levels)
+    print(actualtarget,extra)
     option=[actualtarget]+extra
     random.shuffle(option)
     size=WIDTH/(len(option)+1)
+    xc=100
     for u, img in enumerate(option):
         actor=Actor(img)
         actor.active=True
-        actor.x=(u+1)*size
+        actor.x=(xc)
+        actor.y=(50)   
+        actors.append(actor)
+        xc=xc+400
+falling(1)
+def on_mouse_down(pos):
+    global yw,go
+    for item in actors:
+        if item.collidepoint(pos):
+            if (item.image)==actualtarget:
+                yw=True
+            else:
+                go=True
 pgzrun.go()
